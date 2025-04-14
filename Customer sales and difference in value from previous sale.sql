89152dcb149495a6e5b5fa57179e7384b4e5b32f
dhiraj092 <160793683+dhiraj092@users.noreply.github.com>
2025-04-14 12:08:48 -0400

    Update Customer sales and difference in value from previous sale.sql

--- Customer sales and difference in value from previous sale.sql
+++ Customer sales and difference in value from previous sale.sql
@@ -1,26 +1,36 @@
-# View showing customers sales and include a column which shows the difference in value forom their previous sale? I want to see if new customers who make their first purchase are likely to spend more.
 with main_cte as 
 (
-Select ordernumber, orderdate, customernumber, sum(sales_value) as sales_value
+Select ordernumber, 
+  orderdate, 
+  customernumber, 
+  sum(sales_value) as sales_value
 FROM 
-
-(SELECT t1.orderNumber, orderDate, customerNumber, productCode, quantityOrdered * priceEach as sales_value
+  
+(SELECT t1.orderNumber, 
+  orderDate, 
+  customerNumber, 
+  productCode, 
+  quantityOrdered * priceEach as sales_value
 FROM orders t1
 inner join orderdetails t2
 on t1.orderNumber = t2.orderNumber) main 
 
-group by ordernumber, orderdate, customernumber
+group by ordernumber, 
+  orderdate, 
+  customernumber
 ),
 
 sales_query as 
 (
-select t1.*, customerName, row_number() over (partition by customerName order by orderdate) as purchase_number,
-lag(sales_value) over (partition by customerName order by orderdate) prev_sales_value
+select t1.*, 
+  customerName, 
+  row_number() over (partition by customerName order by orderdate) as purchase_number,
+  lag(sales_value) over (partition by customerName order by orderdate) prev_sales_value
 from main_cte t1
 inner join customers t2
 on t1.customernumber = t2.customernumber
 )
-
-select *, sales_value-prev_sales_value as purchase_value_change 
+select *, 
+  sales_value-prev_sales_value as purchase_value_change 
 from sales_query
 where prev_sales_value is not null 
