e4a0967038832747ac411d66522d54df7b3defa3
dhiraj092 <160793683+dhiraj092@users.noreply.github.com>
2025-04-14 12:59:58 -0400

    Update Commanly purchased items together.sql

--- Commanly purchased items together.sql
+++ Commanly purchased items together.sql
@@ -1,4 +1,4 @@
-with prod_sales as 
+WITH prod_sales AS 
 (
 SELECT orderNumber, 
   t1.productCode, 
@@ -8,11 +8,11 @@ INNER JOIN products t2
 on t1.productCode = t2.productCode
 )
 
-select distinct t1.orderNumber, 
-  t1.productline as product_one, 
-  t2.productline as product_two
-from prod_sales t1
-left join prod_sales t2
-on t1.orderNumber = t2.orderNumber and t1.productLine <> t2.productLine
+SELECT DISTINCT t1.orderNumber, 
+  t1.productline AS product_one, 
+  t2.productline AS product_two
+FROM prod_sales t1
+LEFT JOIN prod_sales t2
+ON t1.orderNumber = t2.orderNumber AND t1.productLine <> t2.productLine
 
 
